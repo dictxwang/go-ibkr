c9cba9cc04af16cd3887521cbb7a5e116c58c138
dictwang <dictwang@163.com>
2026-07-01 16:22:24 +0800

    add trade book listen

--- ws_public_booktrader.go
+++ ws_public_booktrader.go
@@ -1,22 +1,60 @@
 package ibkr
 
+import (
+	"fmt"
+
+	"github.com/gorilla/websocket"
+)
+
 type WebsocketPublicBookTraderParam struct {
-	// TODO
+	AccountId string
+	ContractIds []int
+	Exchange      string
+}
+type WebsocketPublicBookTraderDataItem struct {
+	Row int `json:"row"`
+	Focus int `json:"focus"` //Indicates if the value was marked as the last trade price for the contract.
+	Price string `json:"price"`
+	Ask string `json:"ask,omitempty"`
+	Bid string `json:"bid,omitempty"`
 }
 type WebsocketPublicBookTraderResponse struct {
-	// TODO
+	Topic string `json:"topic"`
+	Data []WebsocketPublicBookTraderDataItem `json:"data"`
 }
 
 func (s *WebsocketPublicService) SubscribeBookTrader(
 	param WebsocketPublicBookTraderParam,
 	handler func(WebsocketPublicBookTraderResponse) error,
 ) (func() error, error) {
-	// TODO
-	return nil, nil
+	
+	for _, contractId := range param.ContractIds {
+		args := fmt.Sprintf("sbd+%s+%d", param.AccountId, contractId)
+		if param.Exchange != "" {
+			args = args + "+" + param.Exchange
+		}
+
+		if err := s.writeMessage(websocket.TextMessage, []byte(args)); err != nil {
+			return nil, err
+		}
+	}
+
+	s.bookTraderResponseHandler = handler
+
+	return func() error {
+		args := fmt.Sprintf("ubd+{%s}", param.AccountId)
+		if err := s.writeMessage(websocket.TextMessage, []byte(args)); err != nil {
+			return err
+		}
+		return nil
+	}, nil
 }
 func (s *WebsocketPublicService) UnsubscribeBookTrader(
 	param WebsocketPublicBookTraderParam,
 ) error {
-	// TODO
+	args := fmt.Sprintf("ubd+{%s}", param.AccountId)
+	if err := s.writeMessage(websocket.TextMessage, []byte(args)); err != nil {
+		return err
+	}
 	return nil
 }
